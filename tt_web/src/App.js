a6432de47e1e295eb07859967edf3204950a6623
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2023-04-22 19:04:49 +0900

    아이콘클릭시 JS파일 열림구현

--- tt_web/src/App.js
+++ tt_web/src/App.js
@@ -1,36 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import { Routes, Route, BrowserRouter, Link } from 'react-router-dom';
+import Capsule from './capsule.js';
+import Community from './community.js';
+import Ranking from './ranking.js';
+import Search from './search.js';
+import Shop from './shop.js';
+import Person from './person.js';
+
 function App() {
+
+  // useState 훅을 불러온다
+  const [selectedIcon, setSelectedIcon] = useState('');
+
+  // selectedIcon과 해당 아이콘을 클릭했을 때 실행되는 함수 handleIconeClick정의
+  const handleIconClick = (iconName) => {
+    setSelectedIcon(iconName);
+  };
+
   return (
-    <div className="container">
-      <div className="header">
-        <div className="header_sort">
-          <img src="/logo.png" alt="logo_error" className='logo'/>
-          <img src='/person.svg' alt='person_error' className='person'/>
+    <BrowserRouter>
+      <div className="container">
+        <div className="header">
+          <div className="header_sort">
+            <Link to="/">
+              <img src="/logo.png" alt="logo_error" className='logo'/>
+            </Link>
+            <Link to="/person">
+              <img src='/person.svg' alt='person_error' className='person'/>
+            </Link>
+          </div>
         </div>
-    </div>
-      <div className="content">Content</div>
-      <div className="footer">
-        <nav className='footer_nav'>
-          <a id='ranking' href=''>
-            <img src="/icon/ranking.png" alt='ranking_err' className='ranking'/>
-          </a>
-          <a id='community' href=''>
-            <img src="/icon/community.png" alt='community_err' className='community'/>
-          </a>
-          <a id='shop' href=''>
-            <img src="/icon/shop.png" alt='shop_err' className='shop'/>
-          </a>
-          <a id='capsule' href=''>
-            <img src="/icon/capsule.png" alt='capsule_err' className='capsule'/>
-          </a>
-          <a id='search' href=''>
-            <img src="/icon/search.png" alt='search_err' className='search'/>
-          </a>
-        </nav>
+        <div className="content">Content</div>
+        <div className="footer">
+          <nav className='footer_nav'>
+            <Link to="/ranking">
+              <img src="/icon/ranking.png" alt='ranking_err' className={selectedIcon === 'ranking' ? 'selectedIcon' : 'ranking'} onClick={() => handleIconClick('ranking')}/>
+            </Link>
+            <Link to="/community">
+              <img src="/icon/community.png" alt='community_err' className={selectedIcon === 'community' ? 'selectedIcon' : 'community'} onClick={() => handleIconClick('community')}/>
+            </Link>
+            <Link to="/shop">
+              <img src="/icon/shop.png" alt='shop_err' className={selectedIcon === 'shop' ? 'selectedIcon' : 'shop'} onClick={() => handleIconClick('shop')}/>
+            </Link>
+            <Link to="/capsule">
+              <img src="/icon/capsule.png" alt='capsule_err' className={selectedIcon === 'capsule' ? 'selectedIcon' : 'capsule'} onClick={() => handleIconClick('capsule')}/>
+            </Link>
+            <Link to="/search">
+              <img src="/icon/search.png" alt='search_err' className={selectedIcon === 'search' ? 'selectedIcon' : 'search'} onClick={() => handleIconClick('search')}/>
+            </Link>
+          </nav>
+        </div>
+        <Routes>
+          <Route path="/ranking" element={<Ranking />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/capsule" element={<Capsule />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/person" element={<Person />} />
+        </Routes>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ tt_web/src/community.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Community() {
+  return (
+    <div className="content">
+      // community 컴포넌트의 컨텐츠
+    </div>
+  );
+}
+
+export default Community;
