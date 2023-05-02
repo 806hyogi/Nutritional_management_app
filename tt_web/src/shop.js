d68d2f11534a635bbe5afdc5f82230eaff7417d2
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2023-05-02 09:52:32 +0900

    shop 제작완료

--- tt_web/src/shop.js
+++ tt_web/src/shop.js
@@ -4,25 +4,37 @@ import './App.css';
 function Shop() {
   return (
     <div className="content">
-      <h1>Shop</h1>
       <div>
-      <div class="product-card">
-        <div class="badge">Hot</div>
-        <div class="product-tumb">
-          <img src="https://i.imgur.com/xdbHo4E.png" alt=""/>
+        <div class="product-card">
+          <div class="product-tumb">
+            <img src="/shop/star.jpg" alt=""/>
+          </div>
+          <div class="product-details">
+            <span class="product-catagory">background-style</span>
+            <h4><a href="">STAR</a></h4>
+            <p>배경에 별을 넣어서 색다른 환경을 경험하세요!</p>
+            <div class="product-bottom-details">
+              <div class="product-price">2 Point</div>
+            </div> 
+          </div>
         </div>
-        <div class="product-details">
-          <span class="product-catagory">Women,bag</span>
-          <h4><a href="">Women leather bag</a></h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!</p>
-          <div class="product-bottom-details">
-            <div class="product-price"><small>$96.00</small>$230.99</div>
+        <div class="product-card">
+          <div class="product-tumb">
+            <img src="/shop/moon.jpg" alt=""/>
+          </div>
+          <div class="product-details">
+            <span class="product-catagory">background-style</span>
+            <h4><a href="">MOON</a></h4>
+            <p>배경에 달을 넣어서 로맨틱한 분위기를 즐겨보세요!</p>
+            <div class="product-bottom-details">
+              <div class="product-price">3 Point</div>
             </div> 
           </div>
         </div>
       </div>
     </div>
   );
+  
 }
 
 export default Shop;
