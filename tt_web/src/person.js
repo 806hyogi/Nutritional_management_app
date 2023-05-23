4099a33f7c62a8a13b68a9665d5cedc6b0c63318
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2023-05-24 02:48:24 +0900

    해상도 수정 + shop 수정

---
+++ tt_web/src/person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Person() {
+  return (
+    <div className="content">
+      <h1>Person</h1>
+    </div>
+  );
+}
+
+export default Person;
