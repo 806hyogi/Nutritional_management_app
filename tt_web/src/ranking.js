42dc08593fe14ec86498a9a983379ab571ae6f11
806hyogi <rhkdgur120@naver.com>
2023-05-25 02:57:24 +0900

    랭킹 기능 구현 완료

--- tt_web/src/ranking.js
+++ tt_web/src/ranking.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function Ranking() {
@@ -11,41 +11,68 @@ function Ranking() {
     { id: 5, name: '사용자5', experiencePoints: 600 },
   ];
 
+/* 1등 왕관 라이트 효과 */
+  useEffect(() => {
+    const imageElements = document.querySelectorAll('.ranking-image img');
+    let isLightOn = false;
+
+    const toggleImageLight = () => {
+      imageElements.forEach((image) => {
+        if (isLightOn) {
+          image.classList.remove('light-on');
+        } else {
+          image.classList.add('light-on');
+        }
+      });
+
+      isLightOn = !isLightOn;
+    };
+
+    const intervalId = setInterval(toggleImageLight, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <div className="content">
       <div className="ranking-title">
-        <h1>랭킹</h1>
-      </div>
 
+      </div>
       <ul>
         {rankings.slice(0, 3).map((user, index) => (
           <li key={user.id}>
-
             {index === 0 && (
-              <img
-                className="ranking-image"
-                src="icon/1.png"
-                alt="사용자1"
-              />
+              <div className="ranking-image">
+                <img
+                  src="icon/1.png"
+                  alt="사용자1"
+                />
+              </div>
             )}
             {index === 1 && (
+              <div className="ranking-image2">
               <img
-                className="ranking-image"
                 src="icon/2.png"
                 alt="사용자2"
               />
+            </div>
             )}
             {index === 2 && (
+              <div className="ranking-image3">
               <img
-                className="ranking-image"
                 src="icon/3.png"
                 alt="사용자3"
               />
+            </div>
             )}
             <div className='ranking'>
-              <span>{user.name}</span>
-              
-              <span> - 경험치: {user.experiencePoints}</span>
+              <span>
+                {user.name}
+                <br/>
+                Exp {user.experiencePoints}
+                </span>
             </div>
           </li>
         ))}
