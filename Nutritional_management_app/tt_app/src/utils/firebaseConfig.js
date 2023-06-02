31195b00725f28024320e67d71a7c94e671f69a0
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2023-06-03 02:32:40 +0900

    형 이거 수정해줘

---
+++ Nutritional_management_app/tt_app/src/utils/firebaseConfig.js
@@ -0,0 +1,48 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, collection, doc, setDoc, getDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAelFFU-J3udH31aa41a2O_f2pSNu6IYa8",
+  authDomain: "teunteun-e58c6.firebaseapp.com",
+  projectId: "teunteun-e58c6",
+  storageBucket: "teunteun-e58c6.appspot.com",
+  messagingSenderId: "973060701436",
+  appId: "1:973060701436:web:c69d5176d2acf9540b6b79",
+  measurementId: "G-7XERBFLCXP"
+};
+
+// Firebase 초기화
+const app = initializeApp(firebaseConfig);
+
+// Firebase 인증 기능 가져오기
+const auth = getAuth();
+
+// Firestore 초기화
+const db = getFirestore();
+
+  // 회원가입 함수
+  export const signup = async ({ nickname, email, password, passwordConfirm }) => {
+  const { user } = await createUserWithEmailAndPassword(auth, email, password);
+
+  // 사용자 정보를 추가합니다. users 컬렉션에 새로운 문서를 생성하고, 문서 내에 필드로 email, password, nickname을 저장합니다.
+  await setDoc(doc(collection(db, "users"), user.uid), {
+    email,
+    password,
+    nickname
+  });
+
+  return user;
+}
+
+  // 로그인 함수
+  export const login = async ({ email, password }) => {
+  const { user } = await signInWithEmailAndPassword(auth, email, password);
+  const idToken = await user.getIdToken();
+  const userRef = doc(db, "users", user.uid);
+  const userDoc = await getDoc(userRef);
+  const userObj = userDoc.data();
+  // 사용자 객체(user)와 ID 토큰(idToken)을 반환합니다.
+  return { user: userObj, idToken };
+};
