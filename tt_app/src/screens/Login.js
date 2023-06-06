285e9e0422a6552f225ca2bbc1368d4c70e1f17a
WoongHL <57064588+MelMory@users.noreply.github.com>
2023-06-06 09:50:44 +0900

    Update Login.js

--- tt_app/src/screens/Login.js
+++ tt_app/src/screens/Login.js
@@ -1,6 +1,6 @@
 // 로그인 화면 컴포넌트
 
-import React, {useState, useRef, useEffect, useContext } from 'react';
+import React, { useState, useRef, useEffect, useContext } from 'react';
 import { ProgressContext } from '../contexts';
 import styled from "styled-components/native";
 import { TouchableOpacity, Text } from 'react-native';
@@ -8,13 +8,9 @@ import { Input, Button } from "../components";
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { validateEmail, removeWhitespace } from '../utils/common';
 import { images } from "../utils/images";
-import {Alert} from 'react-native';
-import {login} from '../utils/firebaseConfig';
+import { Alert } from 'react-native';
+import { login } from '../utils/firebaseConfig';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 54eac6cf40af904dd01bfc0d6d10c108bc6b101e
 // 전체 화면을 이미지로 채우는 역할
 const Background = styled.ImageBackground`
   flex: 1;
@@ -57,92 +53,63 @@ const WhiteText = styled.Text`
   margin-bottom: 10px;
 `;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 54eac6cf40af904dd01bfc0d6d10c108bc6b101e
 // 로그인 컴포넌트에 nevigation 매개변수 받음.
 const Login = ({ navigation }) => {
   // spinner 사용
-  const {spinner} = useContext(ProgressContext);
-  
+  const { spinner } = useContext(ProgressContext);
+
   // 이메일, 비밀번호, 에러메세지, disabled 번수 초기화
-<<<<<<< HEAD
-    const [email, setEmail] = useState("");
-=======
   const [email, setEmail] = useState("");
->>>>>>> 54eac6cf40af904dd01bfc0d6d10c108bc6b101e
-    const [password, setPassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
-    const [disabled, setDisabled] = useState(true);
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [disabled, setDisabled] = useState(true);
 
-    const passwordRef = useRef(null); // useRef 사용.
+  const passwordRef = useRef(null); // useRef 사용.
 
-    // email, password, errorMessage 값이 변경될때마다 disabled 업데이트
-    useEffect(() => {
-      setDisabled(!(email && password && !errorMessage));
-    }, [email, password, errorMessage]);
+  // email, password, errorMessage 값이 변경될때마다 disabled 업데이트
+  useEffect(() => {
+    setDisabled(!(email && password && !errorMessage));
+  }, [email, password, errorMessage]);
 
-    // email 값 변경
-    const _handleEmailChange = (email) => {
-      const changedEmail = removeWhitespace(email); // 공백 제거
-      setEmail(changedEmail);
-      setErrorMessage( // 이메일이 유효하지 않는 경우?
-          validateEmail(changedEmail) ? "" : "이메일을 입력해주세요."
-      );
-    };
-<<<<<<< HEAD
+  // email 값 변경
+  const _handleEmailChange = (email) => {
+    const changedEmail = removeWhitespace(email); // 공백 제거
+    setEmail(changedEmail);
+    setErrorMessage( // 이메일이 유효하지 않는 경우?
+      validateEmail(changedEmail) ? "" : "이메일을 입력해주세요."
+    );
+  };
+  // password 값 변경
+  const _handlePasswordChange = (password) => {
+    setPassword(removeWhitespace(password));
+  };
 
-=======
-    
->>>>>>> 54eac6cf40af904dd01bfc0d6d10c108bc6b101e
-    // password 값 변경
-    const _handlePasswordChange = (password) => {
-      setPassword(removeWhitespace(password));
-    };
-
-<<<<<<< HEAD
-    const _handleLoginButtonPress = async () => {
-      try {
-        spinner.start();
-        const { user, idToken } = await login({ email, password });
-        navigation.navigate('Home', { idToken, user });
-      } catch (e) {
-        Alert.alert('로그인 실패', '이메일과 비밀번호를 다시 확인하세요.');
-      } finally {
-        spinner.stop();
-      }
-    };
-    
-    
-    
-=======
-    const _handleLoginButtonPress = async() => {
-      try {
-        spinner.start(); // spinner 시작
-        const user = await login({email, password}); // 이메일, 비밀번호 인자 전달
-        Alert.alert("로그인 성공", user.email);
-
-      } catch(e) {
-        Alert.alert("로그인 실패", "이메일과 비밀번호를 다시 확인하세요.");
-
-      } finally {
-        spinner.stop(); // spinner 중지
-      }
-    };
->>>>>>> 54eac6cf40af904dd01bfc0d6d10c108bc6b101e
+  const _handleLoginButtonPress = async () => {
+    try {
+      spinner.start();
+      const { user, idToken } = await login({ email, password });
+      navigation.navigate('Home', { idToken, user });
+    } catch (e) {
+      Alert.alert('로그인 실패', '이메일과 비밀번호를 다시 확인하세요.');
+    } finally {
+      spinner.stop();
+    }
+  };
 
 
-// TouchableOpacity 터치하면 signup 스크린으로 이동 (onSubmitEditing에 빈값은 키보드 다음을 눌렀을때 아무일도 일어나지 않게함)
+
+
+
+  // TouchableOpacity 터치하면 signup 스크린으로 이동 (onSubmitEditing에 빈값은 키보드 다음을 눌렀을때 아무일도 일어나지 않게함)
   return (
     <Background source={images.backgroundMe} resizeMode="cover">
       <KeyboardAwareScrollView
-        contentContainerStyle={{ flex:1 }} // 전체화면 가득채움
+        contentContainerStyle={{ flex: 1 }} // 전체화면 가득채움
         extraScrollHeight={20} // 키보드가 열렸을때 추가로 스크롤할 높이 설정
       >
-      <Container>
-        <Title>LOGIN</Title>
-        <Contents>
+        <Container>
+          <Title>LOGIN</Title>
+          <Contents>
             {errorMessage != "" ? <WhiteText>{errorMessage}</WhiteText> : null}
             <Input
               value={email}
