d0319241a0d6243d8c319b8be5ddcac4db7dffe1
806hyogi <rhkdgur120@naver.com>
2023-06-06 15:51:59 +0900

    test

--- Nutritional_management_app/tt_app/README.md
+++
@@ -1,2 +0,0 @@
-# Nutritional_management_app
-[종합프로젝트] 영양제 관리 어플 "튼튼"
--- Nutritional_management_app/tt_app/src/theme.js
+++
@@ -1,6 +0,0 @@
-export const theme = {
-  backgroundColor: '#FFFFFF',
-  main: "#FFA45B",
-  text: "#000000",
-  done: "#CFCFCF"
-}
